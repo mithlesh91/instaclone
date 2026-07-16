1b2b72596ce97304a8491df60331adfe1f83b3a6
Mithlesh <aryanrajnwd9155@gmail.com>
2026-07-16 16:38:31 +0530

    Refactor login and registration forms with state management and improved styling
    Signed-off-by: Mithlesh <aryanrajnwd9155@gmail.com>

--- Frontend/src/features/auth/pages/Loing.jsx
+++ Frontend/src/features/auth/pages/Loing.jsx
@@ -1,20 +1,42 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import '../share/style.scss'
+import { useState, useEffect } from 'react'
 
 const Loing = () => {
+  const [username, setusername] = useState('')
+  const [password, setpassword] = useState("")
+
+   function formhandle(e){
+     e.preventDefault()
+     console.log("username",username)
+     console.log("password",password)
+  }
+
+
+
   return (
-   <main>
-    <div className="form-contener">
-        <h1>Login</h1>
-        <form>
-            <input type="text" placeholder='Username' name='username' />
-            <input type="password" placeholder='Password' name='password' />
-            <button className='button entire-button'>Login</button>
+    <main>
+      <div className="form-contener">
+        <h1>InstaCLone</h1>
+        <form onSubmit={formhandle}>
+          <input
+            value={username}
+            onChange={(e) => { setusername(e.target.value) }}
+            type="text" placeholder='Username or Email'
+            name='username'
+          />
+          <input
+            value={password}
+            onChange={(e) => { setpassword(e.target.value) }}
+            type="password" placeholder='Password'
+            name='password'
+          />
+          <button className='button entire-button'>Log In</button>
         </form>
-          <p>Don't have an account? <Link to="/register">Register</Link></p>
-    </div>
-   </main>
+        <p>Don't have an account? <Link to="/register">Sign up</Link></p>
+      </div>
+    </main>
   )
 }
 
--- Frontend/src/features/auth/pages/Register.jsx
+++ Frontend/src/features/auth/pages/Register.jsx
@@ -1,20 +1,50 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import '../share/style.scss'
+
+import { useState } from 'react'
 
 const Register = () => {
+    const [username, setusername] = useState("")
+    const [email, setemail] = useState("")
+    const [password, setpassword] = useState("")
+
+    function formhandle(e) {
+        e.preventDefault()
+        console.log("username",username)
+        console.log("email",email)
+        console.log("password",password)
+    }
+
+    
   return (
-     <main>
-    <div className="form-contener">
-        <h1>Login</h1>
-        <form>
-            <input type="text" placeholder='Username' name='username' />
-            <input type="text" placeholder='email' name='email'/>
-            <input type="password" placeholder='Password' name='password' />
-            <button type='submit'>Register</button>
+    <main>
+      <div className="form-contener">
+        <h1>Create Account</h1>
+        <form onSubmit={formhandle}>
+          <input 
+          value={username}
+          onChange={(e)=>{setusername(e.target.value)}}
+          type="text" placeholder='Username'
+           name='username'
+            />
+          <input
+           value={email}
+          onChange={(e)=>{setemail(e.target.value)}}
+           type="email" placeholder='Email'
+            name='email' 
+            />
+          <input 
+           value={password}
+          onChange={(e)=>{setpassword(e.target.value)}}
+          type="password" placeholder='Password' 
+          name='password' 
+          />
+          <button type='submit' className='button entire-button'>Sign Up</button>
         </form>
-        <p>Already have an account? <Link to="/login">Login</Link></p>
-    </div>
-   </main>
+        <p>Already have an account? <Link to="/login">Log in</Link></p>
+      </div>
+    </main>
   )
 }
 
